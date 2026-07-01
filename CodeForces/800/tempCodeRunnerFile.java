0eaca551499779dff48732209d5244373fb3d556
Saptarshi <saptarshichowdhury9f43@gmail.com>
2026-07-01 23:58:04 +0530

    today's push

--- CodeForces/800/tempCodeRunnerFile.java
+++ CodeForces/800/tempCodeRunnerFile.java
@@ -1,42 +1,24 @@
 import java.util.Scanner;
 
-public class OneAndTwo {
-    public static void main(String[] args) 
-    {
-        
-    Scanner sc = new Scanner(System.in);
+public class Sum
+{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         int t = sc.nextInt();
-        while((t--) > 0)
+        while(t-- > 0)
         {
-            int n = sc.nextInt();
-            int arr[] = new int[n];
-            int cnt2 = 0;
-            for(int i = 0; i<n; i++)
-            {
-                arr[i] = sc.nextInt();
-                if(arr[i] == 2)
-                    cnt2++;
-            }
-            int count = 0;
-            int index = -1;
-            if(cnt2 == 0)
-                System.out.println(1);
-            else if(cnt2%2 == 1)
-                System.out.println(-1);
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            int c = sc.nextInt();
+            if(a+b == c)
+                System.out.println("YES");
+            else if(b+c == a)
+                System.out.println("YES");
+            else if(a+c == b)
+                System.out.println("YES");
             else
-            {
-                for(int i = 0; i<n; i++)
-                {
-                    if(arr[i] == 2)
-                        count++;
-                    if(count == cnt2/2)
-                    {    index = i;
-                        break;
-                    }
-                }
-                System.out.println(index); 
-            }
+                System.out.println("NO");
         }
         sc.close();
-    }    
+    }
 }
